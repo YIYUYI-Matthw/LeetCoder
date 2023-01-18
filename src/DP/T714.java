995f0cddbdaa6acf66fe39ddcdf47ba4e4ed300e
YIYUYI <ysf456750@163.com>
2023-01-19 00:30:16 +0800

    T309：有冷冻期：会了：找时间考虑下滚动数组 T714：dp和贪心

--- src/DP/T714.java
+++ src/DP/T714.java
@@ -13,6 +13,16 @@ public class T714 {
 class Solution_714_self {
     /*
     【×】一个很朴素的想法：抓住所有超过fee的最大非下降边
+    上升边：profit>fee：积累；else：记0
+    下降边：profit=dp[i-1]，更新买入价
+    关键：如何合并两条上升边：e.g. fee=3：① 1->7赚3；② 7->5赚-2(不计fee)；③ 5->10赚2：1->7+5->10赚5。
+                                    合并：1->10=p(1->7)+p(7->5)+p(5-10)-fee=6 ==> 状态转移
+     每天划分为两种状态：今天处于上升边、今天处于下降边：在趋势交汇处会同时处于两种状态：分别记录就好
+     这里的上升边：指的是非下降边
+     buy_val
+     dp[0][:] = 0;
+     dp[i][0] = delt<0 ? dp[i-1][0]:dp[i-1][0]+delt
+     dp[i][1] = delt<0 ? dp[i-1][1]+prices[i]-prices[i-1] : dp[i-1][1]
      */
     public int maxProfit(int[] prices, int fee) {
         int buyFee = prices[0];
@@ -28,5 +38,55 @@ class Solution_714_self {
             }
             temp_Sell = prices[i];
         }
+        return 0;
+    }
+}
+
+class Solution_T714_org_dp {
+    public int maxProfit(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
+    }
+}
+
+class Solution_T714_org_greedy {
+    public int maxProfit(int[] prices, int fee) {
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+            if (prices[i] + fee < buy)
+                buy = prices[i] + fee;
+        }
+        return profit;
+    }
+}
+
+class Solution_T714_others {
+    public int maxProfit(int[] prices, int fee) {
+        int res = 0;
+        int min = prices[0];//最低价格
+        for (int i = 1; i < prices.length; i++) {
+            //最低价格买入，又更低就记录
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                //有利润卖掉，注意卖完再买回来，因为后面可能有更高的利润
+                if (prices[i] - min - fee > 0) {
+                    res += prices[i] - min - fee;
+                    min = prices[i] - fee;//因为每次计算利润都要算fee，但这次已经算过了，所以这次买就少算一个fee
+                }
+            }
+        }
+        return res;
     }
 }
